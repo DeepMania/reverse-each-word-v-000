eff0dc99a39d02ae3a77c9db0712965bc213f332
Yana Ustsinovich <yustinovich@icloud.com>
2018-06-05 22:59:34 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(text)
-  text_into_array = text.split
+  text_into_array = text.split(" ")
   reversed_text = []
   text_into_array.each do |word|
     reversed_text << word.reverse
